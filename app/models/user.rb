990c69d1e337248b427d59ca7d33a8f1d65685c0
github username <github email address>
2020-04-14 16:50:49 -0400

    added a presnece validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :artworks, dependent: :destroy
   has_many :artists, through: :artworks
   has_many :object_types, through: :artworks
+  validates :name, :email, presence: true
 end
